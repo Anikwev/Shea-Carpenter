b53ab232cd9744a27621091161336b29a1f12773
Tawhid Islam Anik <taohidislamanik92@gmail.com>
2024-09-09 23:00:25 +0600

    Add files via upload

---
+++ src/Layouts/LayoutOne.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { Navbar } from '../components/navbar/Navbar'
+import { useSelector } from 'react-redux'
+
+export const LayoutOne = () => {
+  // =========== get data from redux
+  const sliceUser = useSelector((state)=>state.counter.value)
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(sliceUser == null){
+      navigate('/loginPage')
+    }
+
+  } ,[])
+  console.log(sliceUser)
+  return (
+    <div className='flex'>
+      <Navbar/>
+      <Outlet/>
+    </div>
+  )
+}
